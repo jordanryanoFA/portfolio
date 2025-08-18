7444800a8fa7b4f75c6e8f03c17d587f0ead5409
jordanryanoFA <142567762+jordanryanoFA@users.noreply.github.com>
2025-08-18 16:18:33 +0700

    Update Footer.jsx
    adding yesterday display date and today display date

--- nasa/src/components/Footer.jsx
+++ nasa/src/components/Footer.jsx
@@ -1,15 +1,19 @@
 export default function Footer(props) {
-    const { showModal, handleToggleModal, data } = props
+  const { handleToggleModal, data, onYesterday, onToday } = props
   return (
     <footer>
-        <div className="bgGradient"></div>
-        <div>
-            <h1>APOD PROJECT</h1>
-            <h2>{data?.title}</h2>
-        </div>
+      <div className="bgGradient"></div>
+      <div>
+        <h1>APOD PROJECT</h1>
+        <h2>{data?.title}</h2>
+      </div>
+      <div className="controls">
+        <button onClick={onYesterday}>⬅ Yesterday</button>
+        <button onClick={onToday}>Today</button>
         <button onClick={handleToggleModal}>
-            <i className="fa-solid fa-circle-info"></i>
+          <i className="fa-solid fa-circle-info"></i>
         </button>
+      </div>
     </footer>
   )
 }
